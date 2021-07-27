cd771d787c2182b6f49a025be8bd997372c2fb41
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-07-28 00:14:43 +0430

    add homepage picture

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+import HomeImg from '../assets/imgs/shopp.jpg'
 export function Home() {
     return (
         <>
-            <h1>Welcome to our Shopping Center</h1>
+            <div className="d-flex justify-content-center flex-column align-items-center ">
+                <h1 className="">Welcome to our Shopping Center</h1>
+                <img src={HomeImg} className='w-50 mt-3 ' alt='' />
+            </div>
         </>
     )
 }
